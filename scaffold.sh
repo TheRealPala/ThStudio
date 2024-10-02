3e8ada12dc2967dded2145cf718589388ec006de
pala <apalano23@gmail.com>
2024-10-02 23:58:32 +0200

    FEAT: Added shutdown.sh

--- scaffold.sh
+++ scaffold.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker-compose up -d -f dataDB/docker-compose.yml
+docker-compose -f container_db/docker-compose.yml up -d 
 docker cp container_db/empty_db_dump.sql db_swe:/empty_db_dump.sql
+sleep 0.5
 docker exec -it db_swe mysql -u root -proot -e "source empty_db_dump.sql"

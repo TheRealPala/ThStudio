0b7ea5ac54fdc9b116ca3f34c391d3f8271964a9
pala <apalano23@gmail.com>
2024-10-04 12:00:20 +0200

    FEAT: Added sql dump for db_test

--- scaffold.sh
+++ scaffold.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 docker-compose -f container_db/docker-compose.yml up -d 
-docker cp container_db/empty_db_dump.sql db_swe:/empty_db_dump.sql
+docker cp container_db/th_studio_empty_dump.sql db_swe:/th_studio_empty_dump.sql
+docker cp container_db/th_studio_test_empty_dump.sql db_swe:/th_studio_test_empty_dump.sql
 sleep 0.5
-docker exec -it db_swe mysql -u root -proot -e "source empty_db_dump.sql"
+docker exec -it db_swe mysql -u root -proot -e "source th_studio_empty_dump.sql"
+docker exec -it db_swe mysql -u root -proot -e "source th_studio_test_empty_dump.sql"

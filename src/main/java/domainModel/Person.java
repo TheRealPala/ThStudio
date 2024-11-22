244baf9508b92e905953cc46fee22a7400a0427c
pala <apalano23@gmail.com>
2024-11-22 22:26:38 +0100

    FIX: Removed nested class made by accident

--- src/main/java/domainModel/Person.java
+++ src/main/java/domainModel/Person.java
@@ -61,30 +61,4 @@ public class Person {
     public void setId(int id) {
         this.id = id;
     }
-
-    public static class Tag {
-        protected String tag;
-        protected String tagType;
-
-        public Tag(String tag, String tagType ){
-            this.tag = tag;
-            this.tagType = tagType;
-        };
-
-        public String getTag() {
-            return tag;
-        }
-
-        public void setTag(String tag) {
-            this.tag = tag;
-        }
-
-        public String getTagType() {
-            return tagType;
-        }
-
-        public void setTagType(String tagType) {
-            this.tagType = tagType;
-        }
-    }
 }

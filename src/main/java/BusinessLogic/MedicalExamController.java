8c31aa87ad3a96a2b8ce9bc5fb85f34597af1360
Sabinodm <sabinodemaio@gmail.com>
2024-11-27 00:18:37 +0100

    FEAT: added documentation in MecicalExamController. Resolves #2

--- src/main/java/BusinessLogic/MedicalExamController.java
+++ src/main/java/BusinessLogic/MedicalExamController.java
@@ -21,8 +21,22 @@ public class MedicalExamController {
         this.doctorController = doctorController;
     }
 
-    //TODO aggiungere Doc block
-
+    /**
+     * Adds a new medical exam to the list
+     *
+     * @param idCustomer        The id of the customer
+     * @param idDoctor          The id of the doctor
+     * @param endTime           The end time of the medical exam
+     * @param startTime         The start time of the medical exam
+     * @param description       The description of the medical exam
+     * @param title             The title of the medical exam
+     * @param price             The price of the medical exam
+     *
+     * @return The id of the newly created medical exam
+     *
+     * @throws Exception If the doctor is not found, bubbles up exceptions of MedicalExamDAO::insert()
+     * @throws IllegalArgumentException If the doctor is already occupied in the given time range
+     */
     public int addMedicalExam(int idCustomer, int idDoctor, LocalDateTime endTime, LocalDateTime startTime, String description, String title, double price) throws Exception {
         Doctor doctor = doctorController.getPerson(idDoctor);
         if (doctor == null)
@@ -43,6 +57,20 @@ public class MedicalExamController {
         return medicalExam.getId();
     }
 
+    /**
+     * Updates a medical exam
+     *
+     * @param ExamId            The id of the medical exam
+     * @param idCustomer        The id of the customer
+     * @param idDoctor          The id of the doctor
+     * @param endTime           The end time of the medical exam
+     * @param startTime         The start time of the medical exam
+     * @param description       The description of the medical exam
+     * @param title             The title of the medical exam
+     * @param price             The price of the medical exam
+     *
+     * @throws Exception If the medical exam is not found, bubbles up exceptions of MedicalExamDAO::update()
+     */
     public void updateMedicalExam(int ExamId, int idCustomer, int idDoctor, LocalDateTime endTime, LocalDateTime startTime, String description, String title, double price) throws Exception {
         if (this.medicalExamDao.get(ExamId) == null)
             throw new IllegalArgumentException("Medical Exam not found");
@@ -51,22 +79,56 @@ public class MedicalExamController {
         this.medicalExamDao.update(medicalExam);
     }
 
-    public boolean removeMedicalExam(int id) throws Exception {
-        return medicalExamDao.delete(id);
+    /**
+     * Delete a medical exam
+     *
+     * @param ExamId
+     * @return
+     * @throws Exception
+     */
+    public boolean removeMedicalExam(int ExamId) throws Exception {
+        return medicalExamDao.delete(ExamId);
     }
 
-    public MedicalExam getExam (int id) throws Exception {
-        return medicalExamDao.get(id);
+    /**
+     * Return the given medical exam
+     *
+     * @param ExamId
+     * @return
+     * @throws Exception
+     */
+    public MedicalExam getExam (int ExamId) throws Exception {
+        return medicalExamDao.get(ExamId);
     }
 
+    /**
+     * Return a read-only list of medical exam
+     *
+     * @return
+     * @throws Exception
+     */
     public List<MedicalExam> getAll() throws Exception {
         return unmodifiableList(this.medicalExamDao.getAll());
     }
 
+    /**
+     * Return a read-only list of medical exam for the given doctor
+     *
+     * @param idDoctor
+     * @return
+     * @throws Exception
+     */
     public List<MedicalExam> getDoctorExams(int idDoctor) throws Exception {
         return unmodifiableList(this.medicalExamDao.getDoctorExams(idDoctor));
     }
 
+    /**
+     * Return a read-only list of medical exam for the given customer
+     *
+     * @param idCustomer
+     * @return
+     * @throws Exception
+     */
     public List<MedicalExam> getCustomerExams(int idCustomer) throws Exception {
         return unmodifiableList(this.medicalExamDao.getCustomerExams(idCustomer));
     }

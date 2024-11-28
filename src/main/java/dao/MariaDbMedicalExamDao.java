1484523e1ae19659bf9cc5e4266d4164c8dc2371
Sabinodm <sabinodemaio@gmail.com>
2024-11-28 17:05:17 +0100

    FEAT: added getExamByState in MedicalExamDao

--- src/main/java/dao/MariaDbMedicalExamDao.java
+++ src/main/java/dao/MariaDbMedicalExamDao.java
@@ -255,6 +255,42 @@ public class MariaDbMedicalExamDao implements MedicalExamDao {
     }
 
     @Override
+    public List<MedicalExam> getExamsByState(String state) throws Exception {
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<MedicalExam> mEList = new ArrayList<>();
+        try {
+            con = Database.getConnection();
+            ps = con.prepareStatement("select * from medical_exams where state = ?");
+            ps.setString(1, state);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                MedicalExam tmp = new MedicalExam(
+                        rs.getInt("id"),
+                        rs.getInt("id_customer"),
+                        rs.getInt("id_doctor"),
+                        LocalDateTime.parse(rs.getString("start_time")),
+                        LocalDateTime.parse(rs.getString("end_time")),
+                        rs.getString("description"),
+                        rs.getString("title"),
+                        rs.getDouble("price")
+                );
+                this.setMedicalExamState(rs, tmp);
+                ArrayList<Tag> tags = this.tagDao.getTagsFromMedicalExam(tmp.getId());
+                tmp.setTags(tags);
+                mEList.add(tmp);
+            }
+        } finally {
+            assert rs != null : "ResultSet is Null";
+            rs.close();
+            ps.close();
+            Database.closeConnection(con);
+        }
+        return mEList;
+    }
+
+    @Override
     public List<MedicalExam> search(Search search) throws Exception {
         Connection con = null;
         PreparedStatement ps = null;

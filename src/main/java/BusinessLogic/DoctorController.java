a936467ebbe94a1aa41b077bd83e581b95b9e5f5
Sabinodm <sabinodemaio@gmail.com>
2024-12-01 17:36:34 +0100

    FIX https://github.com/TheRealPala/ProgettoSwe/pull/6\#discussion_r1864435190: removed useless customer id in addPerson method in DoctorController

--- src/main/java/BusinessLogic/DoctorController.java
+++ src/main/java/BusinessLogic/DoctorController.java
@@ -10,12 +10,17 @@ public class DoctorController extends PersonController<Doctor>{
 
     /**
      * Add a new doctor
+     * @param name                          The name of the doctor
+     * @param surname                       The surname of the doctor
+     * @param dateOfBirth                   The date of birth of the doctor
+     * @param iban                          The IBAN of the doctor
+     * @param medicalLicenseNumber          The medical license number of the doctor
      *
      * @return The CF of the newly created doctor
      * @throws Exception bubbles up exceptions of PeopleController::addPerson()
      */
-    public String addPerson(String name, String surname, String dateOfBirth, String iban, int id, String medicalLicenseNumber) throws Exception {
-        Doctor d = new Doctor(name, surname, dateOfBirth, iban, id, medicalLicenseNumber);
+    public String addPerson(String name, String surname, String dateOfBirth, String iban, String medicalLicenseNumber) throws Exception {
+        Doctor d = new Doctor(name, surname, dateOfBirth, iban, medicalLicenseNumber);
         return super.addPerson(d);
     }
 }

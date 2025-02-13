fcf2efdf60fee2bc72f47a4d7addf2adf49e43b1
pala <apalano23@gmail.com>
2025-02-13 23:21:25 +0100

    FIX: Added document properties

--- src/main/java/domainModel/Document.java
+++ src/main/java/domainModel/Document.java
@@ -1,19 +1,32 @@
 package domainModel;
 
 public class Document {
+    private int id;
     private String title;
     private String path;
-    private int id;
+    private Person owner;
+    private Person receiver;
+    private MedicalExam medicalExam;
 
-    public Document (int id, String title, String path) {
+    public Document (int id, String title, String path, Person owner) {
         this.id = id;
         this.title = title;
         this.path = path;
+        this.owner = owner;
     }
 
-    public Document (String title, String path) {
+    public Document (String title, String path, Person owner) {
         this.title = title;
         this.path = path;
+        this.owner = owner;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -30,5 +43,29 @@ public class Document {
 
     public void setPath(String path) {
         this.path = path;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public Person getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Person receiver) {
+        this.receiver = receiver;
+    }
+
+    public MedicalExam getMedicalExam() {
+        return medicalExam;
+    }
+
+    public void setMedicalExam(MedicalExam medicalExam) {
+        this.medicalExam = medicalExam;
     }
 }

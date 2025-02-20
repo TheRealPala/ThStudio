6bebfc522cff96d81d8cc41261ebcf34614d8584
pala <apalano23@gmail.com>
2025-02-20 18:53:59 +0100

    FEAT: Added unit test for doctor controller

--- src/main/java/com/thstudio/project/domainModel/Doctor.java
+++ src/main/java/com/thstudio/project/domainModel/Doctor.java
@@ -1,5 +1,7 @@
 package com.thstudio.project.domainModel;
 
+import java.util.Objects;
+
 public class Doctor extends Person {
     private String medicalLicenseNumber;
 
@@ -8,15 +10,28 @@ public class Doctor extends Person {
         super(id, name, surname, dateOfBirth, balance);
         this.medicalLicenseNumber = medicalLicenseNumber;
     }
+
     public Doctor(String name, String surname, String dateOfBirth, String medicalLicenseNumber, double balance) {
         super(name, surname, dateOfBirth, balance);
         this.medicalLicenseNumber = medicalLicenseNumber;
     }
+
     public String getMedicalLicenseNumber() {
         return medicalLicenseNumber;
     }
+
     public void setMedicalLicenseNumber(String medicalLicenseNumber) {
         this.medicalLicenseNumber = medicalLicenseNumber;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Doctor doctor = (Doctor) o;
+        return doctor.id == this.id && doctor.name.equals(this.name) && doctor.surname.equals(this.surname) &&
+                doctor.dateOfBirth.equals(this.dateOfBirth) && doctor.balance == this.balance &&
+                doctor.medicalLicenseNumber.equals(this.medicalLicenseNumber);
+    }
 }
---
+++ src/test/java/com/thstudio/project/businessLogic/DoctorControllerTest.java
@@ -0,0 +1,62 @@
+package com.thstudio.project.businessLogic;
+
+import com.thstudio.project.dao.*;
+import com.thstudio.project.domainModel.Customer;
+import com.thstudio.project.domainModel.Doctor;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoctorControllerTest {
+    private static DoctorController doctorController;
+
+    @BeforeAll
+    static void setDatabaseSettings() {
+        Dotenv dotenv = Dotenv.configure().directory("config").load();
+        Database.setDbHost(dotenv.get("DB_HOST"));
+        Database.setDbName(dotenv.get("DB_NAME_DEFAULT"));
+        Database.setDbTestName(dotenv.get("DB_NAME_TEST"));
+        Database.setDbUser(dotenv.get("DB_USER"));
+        Database.setDbPassword(dotenv.get("DB_PASSWORD"));
+        Database.setDbPort(dotenv.get("DB_PORT"));
+        assertTrue((Database.testConnection(true, false)));
+        DoctorDao doctorDao = new MariaDbDoctorDao(new MariaDbPersonDao());
+        doctorController = new DoctorController(doctorDao);
+    }
+
+    @Test
+    void addDoctor() throws Exception {
+        Doctor doctorToAdd = doctorController.addDoctor("Marco", "Rossi", "2000-10-01", "MLN-01212", 12000);
+        assertNotNull(doctorToAdd);
+        assertNotEquals(doctorToAdd.getId(), 0);
+        Doctor addedDoctor = doctorController.getPerson(doctorToAdd.getId());
+        assertEquals(doctorToAdd, addedDoctor);
+    }
+
+    @Test
+    void updateDoctor() throws Exception {
+        Doctor doctorToAdd = doctorController.addDoctor("Marco", "Rossi", "2000-10-01", "MLN-01212", 12000);
+        doctorToAdd.setName("Luigi");
+        doctorToAdd.setSurname("Bianchi");
+        doctorToAdd.setDateOfBirth("2003-08-22");
+        doctorToAdd.setBalance(350.55);
+        doctorToAdd.setMedicalLicenseNumber("MLN-23485");
+        doctorController.updateDoctor(doctorToAdd);
+        Doctor updatedDoctor = doctorController.getDoctor(doctorToAdd.getId());
+        assertEquals(doctorToAdd, updatedDoctor);
+    }
+
+
+
+    @AfterEach
+    void flushDb() throws SQLException {
+        Connection connection = Database.getConnection();
+        connection.prepareStatement("delete from people").executeUpdate();
+    }
+}

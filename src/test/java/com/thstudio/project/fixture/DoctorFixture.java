ca0cf672365477c983c7ea5269fe150f9cf55ab2
pala <apalano23@gmail.com>
2025-02-23 08:25:23 +0100

    FEAT: Added doctor fixture gen

---
+++ src/test/java/com/thstudio/project/fixture/DoctorFixture.java
@@ -0,0 +1,15 @@
+package com.thstudio.project.fixture;
+
+import com.github.javafaker.Faker;
+import com.thstudio.project.domainModel.Doctor;
+
+
+public class DoctorFixture {
+    private static final Faker faker = new Faker();
+
+    public static Doctor genDoctor() {
+        Doctor doctor = new Doctor(PersonFixture.genPerson());
+        doctor.setMedicalLicenseNumber(faker.regexify("^MLN-\\d{5}$\n"));
+        return doctor;
+    }
+}
